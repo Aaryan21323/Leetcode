b4bd5f649aa5cf729bb79030d9a6c3aa36136887
Aaryan Bista <aaryanbista31@gmail.com>
2026-02-16 20:29:14 +0545

    Palindrome Number

--- palindromenumber.py
+++ palindromenumber.py
@@ -10,7 +10,20 @@ class Solution:
 class Solution1:
     def palindromeNumber(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
+    
+# Another Way pointer way
 
-x = 121
-sol = Solution()
+class Solution2:
+    def palindromeNumber(self,x:int)->bool:
+        s=str(x)
+        l,r=0,len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            l,r=l+1,r-1
+        return True
+
+
+x = 12321
+sol = Solution2()
 print(sol.palindromeNumber(x))
